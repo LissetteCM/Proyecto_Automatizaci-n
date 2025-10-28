5a5b1426b633585220237736f85390eb341ab1b5
AzureAD\LissetteContrerasMor <lcontrerasm@cruxconsultores.com>
2025-10-27 20:11:57 -0600

    Agregar archivos de proyecto.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Proyecto_Automatización
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
